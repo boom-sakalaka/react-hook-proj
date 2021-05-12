3712fb726ce735850c8dbb0c928547345611ca78
Gu <936285702@qq.com>
2021-05-12 22:47:10 +0800

    提交测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 /*
  * @Author: your name
  * @Date: 2021-05-12 22:12:33
- * @LastEditTime: 2021-05-12 22:38:43
+ * @LastEditTime: 2021-05-12 22:45:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \my-reack-hook\jira\src\App.tsx
  */
-import React from 'react';
-import logo from 'logo.svg';
-import './App.css';
+import React from "react";
+import logo from "logo.svg";
+import "./App.css";
 
+const a = 1;
+const b = 2;
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          
-        </p><a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>Learn React</a></header>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
